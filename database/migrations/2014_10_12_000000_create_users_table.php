1f4e950fd6142f90c7cabbc5466be45f171a346a
Anh Hao <doananhhao01@gmail.com>
2018-05-11 14:38:13 +0700

    Migrate 1

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,16 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('sdt', 12)->unique();
+            $table->boolean('nam')->default(TRUE); //true: nam; false: nu
+            $table->string('diachi');
+            $table->string('avatar'); //tên avt.jpg
+            $table->boolean('trangthai')->default(TRUE); //true: hoat động; false: bị khóa
             $table->rememberToken();
             $table->timestamps();
         });
