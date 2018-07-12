3163b10e575cc0c4bbb99ea85d2317a42ef67d1a
Quân <hpminhquan96@gmail.com>
2018-07-12 23:25:34 +0700

    fix name migration

--- database/migrations/2018_07_10_102836_add_foreign_key_phan_cong2.php
+++ database/migrations/2018_07_10_102836_add_foreign_key_phan_cong2.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddForeignKeyPhanCong extends Migration
+class AddForeignKeyPhanCong2 extends Migration
 {
     /**
      * Run the migrations.
